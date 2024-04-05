35be50350a5433504e918e2fae4ac68188b8bbce
McFlanky <cturjanica02@gmail.com>
2024-04-05 18:15:16 -0400

    setup psql instead of pgadmin for wsl 2 ubuntu, created handler for creating users with uuid, and custom model for User struct and databse user to user json return for psql database setup

--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/McFlanky/blog-aggregator/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
-		log.Fatal("PORT environment variable not set")
+		log.Fatal("PORT environment variable is not found")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL environment variable is not found")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database:", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -33,6 +55,8 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
@@ -41,7 +65,7 @@ func main() {
 	}
 
 	log.Printf("Starting server on :%s", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
